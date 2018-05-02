80a5309e1371ef5ac233f852c9a3c5ba62cb472f
ZhengHui <zheng_hui@stu.xidian.edu.cn>
2018-05-02 21:25:27 +0800

    first commit

--- train/train.sh
+++ train/train.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-
+# train from scratch
 ~/local/caffe/build/tools/caffe train -solver IDN_solver.prototxt -gpu 0 2>&1 | tee -a IDN_x2.log
+
+# train from pretrained model
+#~/local/caffe/build/tools/caffe train -solver IDN_solver.prototxt -weights ../test/caffemodel/IDN_x2.caffemodel -gpu 0 2>&1 | tee -a IDN_x2.log
